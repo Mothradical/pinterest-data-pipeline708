076f3fe0512eff886f4436fcea3f47d33103c96b
El Shute <elshute2@gmail.com>
2024-12-23 12:07:43 +0000

    Adds load_s3_to_databricks, user_posting_emulation.py and .gitignore

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -8,10 +8,11 @@ import json
 import sqlalchemy
 from sqlalchemy import text
 local_path = "C:/Users/elshu/OneDrive/Documents/Data/AiCore_Projects/pinterest-data-pipeline708/db_creds.yaml"
-
+invoke_url_pin = "https://jnh2wjrl1j.execute-api.us-east-1.amazonaws.com/test/topics/1254dc635ec5.pin"
+invoke_url_geo = "https://jnh2wjrl1j.execute-api.us-east-1.amazonaws.com/test/topics/1254dc635ec5.geo"
+invoke_url_user = "https://jnh2wjrl1j.execute-api.us-east-1.amazonaws.com/test/topics/1254dc635ec5.user"
 
 random.seed(100)
-
 
 class AWSDBConnector:
 
@@ -62,15 +63,50 @@ def run_infinite_post_data_loop():
             for row in user_selected_row:
                 user_result = dict(row._mapping)
             
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
+            # print(pin_result)
+            # print(geo_result)
+            # print(user_result)
+
+            # Payload for pin information from Pinterest
+            pin_payload = json.dumps({
+                "records": [
+                    {
+                        "value": {"index": pin_result["index"], "unique_id": pin_result["unique_id"], "title": pin_result["title"], "description": pin_result["description"],
+                        "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"], "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"], 
+                        "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"], "save_location": pin_result["save_location"], "category": pin_result["category"]}
+                    }
+                ]
+            }, default=str)
+    
+            # Payload for the geographical information from Pinterest
+            geo_payload = json.dumps({
+                "records": [
+                    { 
+                        "value": {"ind": geo_result["ind"], "timestamp": geo_result["timestamp"], "latitude": geo_result["latitude"], "longitude": geo_result["longitude"], "country": geo_result["country"]}
+                            }
+                        ]
+                    }, default=str)
+
+
+            # Payload for the user information from Pinterest
+            user_payload = json.dumps({
+                "records": [
+                    {
+                    "value": {"ind": user_result["ind"], "first_name": user_result["first_name"], "last_name": user_result["last_name"], "age": user_result["age"], "date_joined": user_result["date_joined"]}
+                    }
+                ]
+            }, default=str)
+
+            headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+            pin_response = requests.request("POST", invoke_url_pin, headers=headers, data=pin_payload)
+            geo_response = requests.request("POST", invoke_url_geo, headers=headers, data=geo_payload)
+            user_response = requests.request("POST", invoke_url_user, headers=headers, data=user_payload)
+
+            print(pin_response.status_code)
+            print(geo_response.status_code)
+            print(user_response.status_code)
 
 
 if __name__ == "__main__":
     run_infinite_post_data_loop()
     print('Working')
-    
-    
-
-
